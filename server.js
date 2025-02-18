4e17e698e96f791da907c9892c290e1d4ca5287e
Niranjan <officialniranjan10@gmail.com>
2025-02-18 14:51:24 +0530

    hCaptcha error resolved

--- server.js
+++ server.js
@@ -31,26 +31,26 @@ app.use(
     })
 );
 
-app.post("/apis/verify-captcha", async (req, res) => {
-    const { hCaptchaToken } = req.body;
+// app.post("/apis/verify-captcha", async (req, res) => {
+//     const { hCaptchaToken } = req.body;
 
-    console.log("Received hCaptcha Token:", hCaptchaToken);
+//     console.log("Received hCaptcha Token:", hCaptchaToken);
 
-    try {
-        const response = await axios.post(
-            "https://hcaptcha.com/siteverify",
-            `secret=ES_70e56ba5c83a4c40b04832065d837f64&response=${hCaptchaToken}`,
-            { headers: { "Content-Type": "application/x-www-form-urlencoded" } }
-        );
+//     try {
+//         const response = await axios.post(
+//             "https://hcaptcha.com/siteverify",
+//             `secret=ES_70e56ba5c83a4c40b04832065d837f64&response=${hCaptchaToken}`,
+//             { headers: { "Content-Type": "application/x-www-form-urlencoded" } }
+//         );
 
-        console.log("hCaptcha API Response:", response.data);
+//         console.log("hCaptcha API Response:", response.data);
 
-        res.send({ success: response.data.success });
-    } catch (error) {
-        console.error(error);
-        res.status(500).send({ error: "Failed to verify captcha" });
-    }
-});
+//         res.send({ success: response.data.success });
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).send({ error: "Failed to verify captcha" });
+//     }
+// });
 
 app.use("/", mainRouter);
 
